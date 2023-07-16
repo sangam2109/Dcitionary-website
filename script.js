664c252785dd4b9d8c88c124ae897e478c0ea3fb
Sangam Arora <arorasam2109@gmail.com>
2023-07-16 11:06:09 +0530

    cases handles

--- script.js
+++ script.js
@@ -8,84 +8,70 @@ function playSpeech() {
     window.speechSynthesis.speak(currentSpeech);
 
 }
-
-const generateLi = (array, i) => {
-    setTimeout(() => {
-    const meaning_list = document.querySelectorAll('.meaning-list')[i];
-    meaning_list.innerHTML = `<li class="meaning-item"> ${array[0].definition}</li>`
-    for (let index = 1; index < array.length; index++) {
-        meaning_list.innerHTML += `<li class="meaning-item"> ${array[index].definition}</li>`
-    }
-},1)
-}
-
-
-
 const fetchDataFromDictionaryAPI = async () => {
-
     try {
         const wordInput = document.getElementById("search-input");
         const response = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}`).catch((error) => {
             document.getElementById('dictionary').innerHTML = `<section id="no-definitions-section" class="flex flex-col items-center justify-center mt-8"><h2 class="font-bold text-3xl mb-4">No Definitions Found</h2><p class="text-lg">Oops! We couldn't find any definitions for the searched word 😐 . </p></section>`;
             return;
-        })
+        });
         const data = response.data[0];
-        console.log(data)
         const searchName = document.querySelector("#search-input").value;
+        console.log(data)
         const content = `
-        <!-- Main Content -->
-        <div id="word">
-            <h1 id="word-display">${data.word ? data.word : searchName}</h1>
-            <button id="play-btn" onclick="playSpeech()"><img src="images/icon-play.svg" alt="" id="play"></button>
-        </div>
-        <p id="phonetic">${data.phonetics && data.phonetics.length > 1 && data.phonetics[1].text ? data.phonetics[1].text : data.phonetics && data.phonetics.length > 0 ? data.phonetics[0].text : data.phonetic ? data.phonetic : ''}</p>
-        <section class="noun-container">
-            <div class="noun-container_data">
-                <h2 class="noun-container_data_noun">noun</h2>
-                <div class="noun-container_data_noun-line"></div>
-            </div>
-            <h4 class="meaning-title">Meaning</h4>
-            <ul class="meaning-list">
-         ${generateLi(data.meanings[0].definitions, 0)}
-            </ul>
-        </section>
-         <section class="synonyms-section">
-            <h2 class="synonyms-title">${data.meanings[0].synonyms.length != 0 ? 'Synonyms' : ''}</h2>
-            <form method="post" action="/?/search">
-                <input type="hidden" name="word" value="electronic keyboard">
-                <button class="synonym-button">${data.meanings[0].synonyms}</button>
-            </form>
-        </section> 
-        <section class="meaning-section">
-            <div class="noun-container_data">
-                <h2 class="noun-container_data_noun">verb</h2>
-                <div class="noun-container_data_noun-line"></div>
-            </div>
-            <h4 class="meaning-title">Meaning</h4>
-            <ul class="meaning-list">
-         ${generateLi(data.meanings[1].definitions, 1)}
-            </ul>
-        </section>
- <section class="synonyms-section">
-            <h2 class="synonyms-title">${data.meanings[1].synonyms.length != 0 ? 'Synonyms' : ''}</h2>
-            <form method="post" action="/?/search">
-                <input type="hidden" name="word" value="electronic keyboard">
-                <button class="synonym-button">${data.meanings[1].synonyms}</button>
-            </form>
-        </section>
-    <section class="section-divider"></section>
-    <div class="source-section">
+      <!-- Main Content -->
+      <div id="word">
+        <h1 id="word-display">${data.word ? data.word : searchName}</h1>
+        <button id="play-btn" onclick="playSpeech()"><img src="images/icon-play.svg" alt="" id="play"></button>
+      </div>
+      <p id="phonetic">${data.phonetics && data.phonetics.length > 1 && data.phonetics[1].text ? data.phonetics[1].text : data.phonetics && data.phonetics.length > 0 ? data.phonetics[0].text : data.phonetic ? data.phonetic : ''}</p>
+      ${generateSections(data.meanings)}
+      <section class="section-divider"></section>
+      <div class="source-section">
         <h2 class="source-title">Source</h2>
-        <a class="source-link"
-            href=${data.source ? data.source : (data.sourceUrls ? data.sourceUrls : "https://en.m.wiktionary.org/wiki/" + searchName)}> ${data.source || data.sourceUrls ? (data.source || data.sourceUrls) : "https://en.m.wiktionary.org/wiki/" + searchName}</a>
+        <a class="source-link" href="${data.source ? data.source : (data.sourceUrls ? data.sourceUrls : "https://en.m.wiktionary.org/wiki/" + searchName)}">${data.source || data.sourceUrls ? (data.source || data.sourceUrls) : "https://en.m.wiktionary.org/wiki/" + searchName}</a>
+      </div>`;
 
-    </div>`
         document.getElementById('dictionary').innerHTML = content;
-        const get=document.getElementById("word-display").innerHTML
-        console.log(get);
     } catch (error) {
         console.error('Error fetching data from the API:', error.message);
     }
+};
+
+const generateSections = (meanings) => {
+    return meanings.map((meaning, i) => {
+        return `
+      <section class="meaning-section">
+        <div class="noun-container_data">
+          <h2 class="noun-container_data_noun">${meaning.partOfSpeech}</h2>
+          <div class="noun-container_data_noun-line"></div>
+        </div>
+        <h4 class="meaning-title">Meaning</h4>
+        <ul class="meaning-list">
+          ${generateLi(meaning.definitions)}
+        </ul>
+        <section class="synonyms-section">
+          <h2 class="synonyms-title">${meaning.synonyms.length > 0 ? 'Synonyms' : ''}</h2>
+          <form method="post" action="/?/search">
+            <input type="hidden" name="word" value="electronic keyboard">
+            ${generateSynonymsButtons(meaning.synonyms)}
+          </form>
+        </section>
+      </section>`;
+    }).join('');
+};
+
+const generateLi = (definitions) => {
+    return definitions.map(definition => {
+        const example = definition.example ? `<p class="example">${definition.example}</p>` : '';
+        return `<li class="meaning-item">${definition.definition}${example}</li>`;
+    }).join('');
+};
+
+const generateSynonymsButtons = (synonyms) => {
+    return synonyms.map(synonym => {
+        return `<button class="synonym-button">${synonym}</button>`;
+    }).join(' ; ');
 };
 
 function changeMode() {
